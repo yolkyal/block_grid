1f26f64fdf7005a7c30a2bc6d2042c7e17589852
yolkyal <=>
2021-11-16 15:58:18 +0000

    Reverting changes to base height, deprioritising as it does not seem useful and it is adding unnecessary complication

--- block_grid/src/block.py
+++ block_grid/src/block.py
@@ -4,23 +4,23 @@ class Block:
 	RIGHT = 2
 	BOTTOM = 3
 
-	def __init__(self, vertex_heights=[(0, 1), (0, 1), (0, 1), (0, 1)]):
+	def __init__(self, vertex_heights=[1, 1, 1, 1]):
 		self.vertex_heights = vertex_heights
 
 	def raise_(self, x):
-		return Block(vertex_heights=[(height[0], height[1] + x) for height in self.vertex_heights])
+		return Block(vertex_heights=[height + x for height in self.vertex_heights])
 
 	def lower(self, x):
-		return Block(vertex_heights=[(height[0], max(height[1] - x, height[0])) for height in self.vertex_heights])
+		return Block(vertex_heights=[max(height - x, 0) for height in self.vertex_heights])
 
 	def raise_point(self, i, x):
 		vertex_heights = list.copy(self.vertex_heights)
-		vertex_heights[i] = (vertex_heights[i][0], vertex_heights[i][1] + x)
+		vertex_heights[i] = vertex_heights[i] + x
 		return Block(vertex_heights=vertex_heights)
 
 	def lower_point(self, i, x):
 		vertex_heights = list.copy(self.vertex_heights)
-		vertex_heights[i] = (vertex_heights[i][0], max(vertex_heights[i][1] - x, vertex_heights[i][0]))
+		vertex_heights[i] = max(vertex_heights[i] - x, 0)
 		return Block(vertex_heights=vertex_heights)
 
 	def copy(self):
--- block_grid/test/test_block.py
+++ block_grid/test/test_block.py
@@ -8,39 +8,39 @@ class TestBlock(unittest.TestCase):
 
 	def testBlock(self):
 		for i in range(4):
-			self.assertEqual((0, 1), self.block.vertex_heights[i])
+			self.assertEqual(1, self.block.vertex_heights[i])
 
 	def testRaise(self):
 		block = self.block.raise_(1)
 		for i in range(4):
-			self.assertEqual((0, 2), block.vertex_heights[i])
+			self.assertEqual(2, block.vertex_heights[i])
 
 	def testLower(self):
 		block = self.block.lower(1)
 		for i in range(4):
-			self.assertEqual((0, 0), block.vertex_heights[i])
+			self.assertEqual(0, block.vertex_heights[i])
 
 	def testLowerBounding(self):
-		self.block.vertex_heights = [(0, 0), (0, 0), (0, 0), (0, 0)]
+		self.block.vertex_heights = [0, 0, 0, 0]
 		block = self.block.lower(1)
 		for i in range(4):
-			self.assertEqual((0, 0), block.vertex_heights[i])
+			self.assertEqual(0, block.vertex_heights[i])
 
 	def testRaisePoint(self):
 		block = self.block.raise_point(0, 1)
-		self.assertEqual((0, 2), block.vertex_heights[0])
-		self.assertEqual((0, 1), block.vertex_heights[1])
-		self.assertEqual((0, 1), block.vertex_heights[2])
-		self.assertEqual((0, 1), block.vertex_heights[3])
+		self.assertEqual(2, block.vertex_heights[0])
+		self.assertEqual(1, block.vertex_heights[1])
+		self.assertEqual(1, block.vertex_heights[2])
+		self.assertEqual(1, block.vertex_heights[3])
 
 	def testLowerPoint(self):
 		block = self.block.lower_point(0, 1)
-		self.assertEqual((0, 0), block.vertex_heights[0])
-		self.assertEqual((0, 1), block.vertex_heights[1])
-		self.assertEqual((0, 1), block.vertex_heights[2])
-		self.assertEqual((0, 1), block.vertex_heights[3])
+		self.assertEqual(0, block.vertex_heights[0])
+		self.assertEqual(1, block.vertex_heights[1])
+		self.assertEqual(1, block.vertex_heights[2])
+		self.assertEqual(1, block.vertex_heights[3])
 
 	def testLowerPointBounding(self):
-		self.block.vertex_heights = [(0, 0), (0, 0), (0, 0), (0, 0)]
+		self.block.vertex_heights = [0, 0, 0, 0]
 		block = self.block.lower_point(0, 1)
-		self.assertEqual((0, 0), block.vertex_heights[0])
+		self.assertEqual(0, block.vertex_heights[0])
