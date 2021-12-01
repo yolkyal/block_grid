85cac04ed81181fbbfa857bc3d9d28abbc47636d
yolkyal <yolkyal@hotmail.co.uk>
2021-12-01 18:23:15 +0000

    Minor cleanup to save loader

--- block_grid/src/block_grid_save_loader.py
+++ block_grid/src/block_grid_save_loader.py
@@ -8,18 +8,18 @@ class BlockGridSaveLoader:
 	def __init__(self, dest_folder):
 		self.dest_folder = dest_folder 
 
-	def save(self, bg, filename):
-		with open(f"{self.dest_folder}/{filename}.bg", "w") as f:
-			f.write(json.dumps(bg, cls=BlockGridEncoder))
+	def save(self, block_grid, filename):
+		with open(f"{self.dest_folder}/{filename}.bg", 'w') as f:
+			f.write(json.dumps(block_grid, cls=BlockGridEncoder))
 
 	def load(self, filename):
-		with open(f"{self.dest_folder}/{filename}.bg") as f:
+		with open(f"{self.dest_folder}/{filename}.bg", 'r') as f:
 			return json.load(f, cls=BlockGridEncoder)
 
 
 class BlockGridEncoder(json.JSONEncoder):
-		def default(self, bg):
-			return {str(item[0]) : str(item[1].vertex_heights) for item in bg.blocks.items()}
+		def default(self, block_grid):
+			return {str(item[0]) : str(item[1].vertex_heights) for item in block_grid.blocks.items()}
 
 		def decode(self, block_grid_json):
 			str_map = json.loads(block_grid_json)
--- block_grid/test/test_block_grid_save_loader.py
+++ block_grid/test/test_block_grid_save_loader.py
@@ -12,17 +12,21 @@ class TestBlockGridSaveLoader(unittest.TestCase):
 
 	@mock.patch("builtins.open", new_callable=mock.mock_open, read_data="{\"(0, 0)\": \"[1, 1, 1, 1]\"}")
 	def testLoad(self, mock_open):
+		# GIVEN
 		result = self.block_grid_save_loader.load(self.block_grid_name)
 
-		mock_open.assert_called_once_with(self.dest_folder + '/' + self.block_grid_name + '.bg')
+		# WHEN
+		mock_open.assert_called_once_with(self.dest_folder + '/' + self.block_grid_name + '.bg', 'r')
+		
+		# THEN
 		self.assertEqual(len(result.blocks), 1)
 		self.assertEqual([1, 1, 1, 1], result.blocks.get((0, 0)).vertex_heights)
 
 	@mock.patch("builtins.open", new_callable=mock.mock_open)
 	def testSave(self, mock_open):
-		self.mock_file = mock_open.return_value
-
+		# WHEN
 		self.block_grid_save_loader.save(self.block_grid, self.block_grid_name)
 
+		# THEN
 		mock_open.assert_called_once_with(self.dest_folder + '/' + self.block_grid_name + '.bg', 'w')
-		self.mock_file.write.assert_called_once_with("{\"(0, 0)\": \"[1, 1, 1, 1]\"}")
+		mock_open.return_value.write.assert_called_once_with("{\"(0, 0)\": \"[1, 1, 1, 1]\"}")
